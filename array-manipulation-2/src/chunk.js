4b2435471a2e0af3720c12d0037426399faf05dd
Addy Abid <addy.abeed@gmail.com>
2021-10-18 16:48:32 -0700

    Completed Array Manipulation 2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,19 @@
 /* exported chunk */
+/*
+test with [foo, bar, baz, qux] size 2
+
+need storage for output
+look at how many indexes in array
+add array to new array starting from first up to given size
+if added partial size so far, then add again starting from current index to index plus given size
+add new array to storage
+return storage
+*/
+function chunk(array, size) {
+  var outPut = [];
+  for (var i = 0; i < array.length; i += size) {
+    var newArr = array.slice(i, i + size);
+    outPut.push(newArr);
+  }
+  return outPut;
+}
