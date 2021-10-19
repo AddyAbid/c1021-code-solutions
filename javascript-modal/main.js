8fb9768215bec78d782da3dedc08bcb0fe614f54
Addy Abid <addy.abeed@gmail.com>
2021-10-19 12:50:41 -0700

    Revised Modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -3,10 +3,15 @@ var $modal = document.querySelector('.absolute');
 var $noButton = document.querySelector('.button-no');
 $openModal.addEventListener('click', toggleModal);
 $noButton.addEventListener('click', toggleModal);
+var tracker = true;
+
 function toggleModal(event) {
-  if ($modal.className === 'absolute hidden') {
+
+  if (tracker === true) {
     $modal.classList.remove('hidden');
+    tracker = false;
   } else {
+    tracker = true;
     $modal.classList.add('hidden');
   }
 }
