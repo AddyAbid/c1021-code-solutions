322cdcaae98375452e333e5e0b26acffef21d76f
Addy Abid <addy.abeed@gmail.com>
2021-10-19 17:20:19 -0700

    Completed Javascript Typing Tutor

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,18 @@
+var $span = document.querySelectorAll('span');
+
+document.addEventListener('keydown', typeKeys);
+var currentLetter = 0;
+function typeKeys(event) {
+
+  if ($span[currentLetter].textContent === event.key || $span[currentLetter].textContent === event.key + ' ') {
+    $span[currentLetter].classList.remove('red');
+    $span[currentLetter].classList.remove('underline-red');
+    $span[currentLetter].classList.add('green');
+    $span[currentLetter].classList.remove('underline');
+    currentLetter++;
+    $span[currentLetter].classList.add('underline');
+  } else {
+    $span[currentLetter].classList.add('red');
+    $span[currentLetter].classList.add('underline-red');
+  }
+}
