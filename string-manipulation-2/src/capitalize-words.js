e03adb884c870eb947d441e3e7daf870d6f34be7
Addy Abid <addy.abeed@gmail.com>
2021-10-20 12:24:18 -0700

    in progress

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,12 +1,20 @@
 /* exported capitalizeWords */
+/*
+create storage to hold given string and set all characters to lowercase
+split each word up by space and add to new storage.
+look at each character in storage and assign to new variable
+declare new variable
+add word with first letter uppercase and every lowercase letter after
+return storage.
+*/
+
 function capitalizeWords(string) {
   var copied = string.toLowerCase();
-
+  var final;
   var stringSplit = copied.split(' ');
   for (var i = 0; i < stringSplit.length; i++) {
     var total = stringSplit[i];
 
-    var final;
     final += total[0].toUpperCase() + total.substr(1);
   }
   return final;
--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,8 +1,16 @@
 /* exported swapChars */
 /*
-
+convert given string into array
+assign first index to second
+and second index to first
+join array
+return string
 */
 
 function swapChars(firstIndex, secondIndex, string) {
-
+  var arr = string.split(' ');
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = arr[firstIndex];
+  var outPut = arr.join(' ');
+  return outPut;
 }
