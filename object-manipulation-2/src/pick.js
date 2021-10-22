b1bb48268241c80819441b2dfe0005821cf3c0e4
Addy Abid <addy.abeed@gmail.com>
2021-10-22 16:56:28 -0700

    in progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -5,16 +5,17 @@
 
 // function pick(source, keys) {
 //   var outPut = {};
-//   var copied = Object.entries(source);
-//   for (var key in copied) {
+//   var convertedObject = Object.entries(source);
 
-//     for (var i = 0; i < keys.length; i++) {
-//       if (keys[i] === copied[key][0]) {
-//         var total = outPut[keys[i]] = copied[key][1];
-//         console.log(total);
+//   for (var i = 0; i < convertedObject.length; i++) {
+//     for (var j = 0; j < keys.length; j++) {
+//       if (keys[j] === convertedObject[i][0]) {
+//         console.log(convertedObject[i][1]);
+//         outPut[keys[j]] = convertedObject[i][1];
+//         return outPut;
 //       }
 //     }
 
 //   }
-//   return total;
+
 // }
