9a1c2e5e403da33bb05e67b25f60020513aed419
Addy Abid <addy.abeed@gmail.com>
2021-11-01 17:55:51 -0700

    Completed String Manipulation 2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,16 +1,18 @@
 /* exported swapChars */
 /*
-convert given string into array
-assign first index to second
-and second index to first
-join array
-return string
+convert given string into array and assign to storage
+create new storage to hold the arrays first index value
+assign second index to first index
+assign second index to new storage
+join the values together and convert to string data type
+return converted array
 */
 
 function swapChars(firstIndex, secondIndex, string) {
-  var converted = string.split();
-  var outPut = converted[firstIndex];
+  var converted = string.split('');
+  var storage = converted[firstIndex];
   converted[firstIndex] = converted[secondIndex];
-  converted[secondIndex] = outPut;
-  return outPut;
+  converted[secondIndex] = storage;
+  converted = converted.join('').toString();
+  return converted;
 }
