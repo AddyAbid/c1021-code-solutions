f9199be84b40b370b3a462190f8039395d05a9b1
Addy Abid <addy.abeed@gmail.com>
2021-11-02 09:57:03 -0700

    in prorgress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -3,19 +3,14 @@
 
 */
 
-// function pick(source, keys) {
-//   var outPut = {};
-//   var convertedObject = Object.entries(source);
-
-//   for (var i = 0; i < convertedObject.length; i++) {
-//     for (var j = 0; j < keys.length; j++) {
-//       if (keys[j] === convertedObject[i][0]) {
-//         console.log(convertedObject[i][1]);
-//         outPut[keys[j]] = convertedObject[i][1];
-//         return outPut;
-//       }
-//     }
-
-//   }
-
-// }
+function pick(source, keys) {
+  var outPut = {};
+  for (var key in source) {
+    for (var i = 0; i < keys.length; i++) {
+      if (key === keys[i]) {
+        outPut[keys[i]] = source[key];
+      }
+    }
+  }
+  return outPut;
+}
