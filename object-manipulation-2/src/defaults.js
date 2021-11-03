26678fee4f19230805a98b5f45730b2d12b63d1a
Addy Abid <addy.abeed@gmail.com>
2021-11-03 10:59:38 -0700

    Completed Object Manipulation 2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,7 +1,13 @@
 /* exported defaults */
 /*
-
+look at each key in given source object
+if that key is not in given target object
+assign source keys to target keys keeping the original target values
 */
 function defaults(target, source) {
-
+  for (var key in source) {
+    if (!(key in target)) {
+      target[key] = source[key];
+    }
+  }
 }
