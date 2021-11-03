0e4e9ff8d87c6c9eec8cba7b59ac7b821736e734
Addy Abid <addy.abeed@gmail.com>
2021-11-03 10:00:33 -0700

    in progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,15 +1,17 @@
 /* exported pick */
 /*
-
+create empty object storage
+look at all keys in given object
+if given array values match the keys in given object and is not equal to undefined
+add only those keys and values to empty object
+return object
 */
 
 function pick(source, keys) {
   var outPut = {};
   for (var key in source) {
-    for (var i = 0; i < keys.length; i++) {
-      if (key === keys[i]) {
-        outPut[keys[i]] = source[key];
-      }
+    if (keys.includes(key) && source[key] !== undefined) {
+      outPut[key] = source[key];
     }
   }
   return outPut;
