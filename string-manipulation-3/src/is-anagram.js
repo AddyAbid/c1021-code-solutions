33f1d791f8f3119e3c5692dfafc13323d3a88b4a
Addy Abid <addy.abeed@gmail.com>
2021-11-03 20:17:18 -0700

    in progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -2,17 +2,20 @@
 /*
 assign first given string to variable and split each word by letter, convert to array, alphebetize, then join characters to remove all spaces
 assign second given string to variable and split each word by letter, convert to array, alphebetize, then join characters to remove all spaces
+split new sorted string by spaces and convert back to string
+split new sorted string by spaces and convert back to string
 compare the first string to the second
-if they are eqaul return true
+if they are not eqaul return false
 else
-return false
+return true
 */
 
 function isAnagram(firstString, secondString) {
 
-  var secondSplit = secondString.split(' ').join('').sort();
-  var firstSplit = firstString.split(' ').join('').sort();
-
+  var secondSplit = secondString.split('').sort().join('');
+  var firstSplit = firstString.split('').sort().join('');
+  firstSplit = firstSplit.split(' ').join('');
+  secondSplit = secondSplit.split(' ').join('');
   if (secondSplit !== firstSplit) {
     return false;
   }
