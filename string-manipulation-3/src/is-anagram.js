885d510172a5402e34c68cc9db0794580fdddc20
Addy Abid <addy.abeed@gmail.com>
2021-11-03 17:56:08 -0700

    in progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,10 +1,20 @@
 /* exported isAnagram */
-function isAnagram(firstString, secondString) {
-  var firstSplit = firstString.split('').sort().join('');
-  var secondSplit = secondString.split('').sort().join('');
+/*
+assign first given string to variable and split each word by letter, convert to array, alphebetize, then join characters to remove all spaces
+assign second given string to variable and split each word by letter, convert to array, alphebetize, then join characters to remove all spaces
+compare the first string to the second
+if they are eqaul return true
+else
+return false
+*/
 
-  if (firstSplit === secondSplit) {
-    return true;
+function isAnagram(firstString, secondString) {
+
+  var secondSplit = secondString.split(' ').join('').sort();
+  var firstSplit = firstString.split(' ').join('').sort();
+
+  if (secondSplit !== firstSplit) {
+    return false;
   }
-  return false;
+  return true;
 }
