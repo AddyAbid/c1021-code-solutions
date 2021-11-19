5b30466468a267a09ba6d6e9aebf8bc90b61bba9
Addy Abid <addy.abeed@gmail.com>
2021-11-19 19:43:20 +0000

    in progress

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,15 +1,45 @@
 /* exported zip */
+/*
+if the lenght of the arrays are not equal remove last index
+push arrays into storage
+search through each value by 2
+push to output storage
+ */
 function zip(first, second) {
-  var total = [];
-  var m = [];
-  first.forEach(function (i, j) {
-    total.push(i);
-    total.push(second[j]);
-  });
-  for (var i = 0; i < total.length; i += 2) {
-    var newArr = total.slice(i, i + 2);
-    m.push(newArr);
+  var temporaryArr = [];
+  var output = [];
+  if (first.length > second.length) {
+    first.pop();
+  } else if (first.length < second.length) {
+    second.pop();
+  }
+  temporaryArr.push(first);
+  temporaryArr.push(second);
+
+  for (var i = 0; i < temporaryArr.length; i += 2) {
+    output.push(temporaryArr.slice(i, i + 2));
+
   }
 
-  return m;
 }
+
+// function zip(first, second) {
+//   var total = [];
+//   var m = [];
+//   if (first.length === second.length) {
+//     second.pop();
+//     console.length(second);
+//   }
+//   first.forEach(function (i, j) {
+//     total.push(i);
+//     total.push(second[j]);
+//   });
+//   for (var i = 0; i < total.length; i += 2) {
+//     var newArr = total.slice(i, i + 2);
+//     m.push(newArr);
+//     console.log(newArr);
+
+//   }
+
+//   return m;
+// }
