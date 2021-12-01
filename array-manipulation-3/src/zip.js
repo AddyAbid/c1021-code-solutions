5c5dc27e5c17a7f6e636f72d386b3d03760bae6b
Addy Abid <addy.abeed@gmail.com>
2021-12-01 01:03:56 +0000

    in progress

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -6,17 +6,33 @@ search through each value by 2
 push to output storage
  */
 function zip(first, second) {
-  var temporaryArr = [];
+  var firstTemp;
+  var secondTemp;
   var output = [];
+  var finalOutput = [];
   if (first.length > second.length) {
     first.pop();
   } else if (first.length < second.length) {
     second.pop();
   }
-  temporaryArr.push(first);
-  temporaryArr.push(second);
-  for (var i = 0; i < temporaryArr.length; i++) {
-    output.push(temporaryArr[i]);
-    // console.log(output);
+  firstTemp = first;
+  secondTemp = second;
+
+  for (var i = 0; i < firstTemp.length; i++) {
+
+    for (var j = 0; j < secondTemp.length; j++) {
+
+      if (firstTemp.indexOf(firstTemp[i]) === secondTemp.indexOf(secondTemp[j])) {
+        output.push(firstTemp[i]);
+        output.push(secondTemp[j]);
+      }
+    }
   }
+  for (var k = 0; k < output.length; k += 2) {
+    var newArr = output.slice(k, k + 2);
+
+    finalOutput.push(newArr);
+  }
+  // console.log(finalOutput);
+  return finalOutput;
 }
