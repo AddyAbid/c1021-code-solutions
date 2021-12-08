a6fde0740ed9dbbdc672f47250005d020d8823fe
Addy Abid <addy.abeed@gmail.com>
2021-12-08 01:21:28 +0000

    double checking work

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,10 +1,25 @@
 /* exported union */
-/*
 
+/*
+create new array for output
+combine both given arrays
+look at each value in combined array
+if output does not include that value
+add it
+otherwise
+remove that value
+return output
  */
 function union(first, second) {
-  const firstArr = first.filter(index => second.includes(index));
-  const secondArr = second.filter(index => !first.includes(index));
-  const concatArr = firstArr.concat(secondArr);
-  return concatArr;
+  var outPut = [];
+  var concatArr = first.concat(second);
+  for (var i = 0; i < concatArr.length; i++) {
+    if (!outPut.includes(concatArr[i])) {
+      outPut.push(concatArr[i]);
+    } else {
+      outPut.splice(i, 1);
+    }
+
+  }
+  return outPut;
 }
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -5,34 +5,16 @@ push arrays into storage
 search through each value by 2
 push to output storage
  */
+
 function zip(first, second) {
-  var firstTemp;
-  var secondTemp;
-  var output = [];
-  var finalOutput = [];
+  var outPut = [];
   if (first.length > second.length) {
     first.pop();
   } else if (first.length < second.length) {
     second.pop();
   }
-  firstTemp = first;
-  secondTemp = second;
-
-  for (var i = 0; i < firstTemp.length; i++) {
-
-    for (var j = 0; j < secondTemp.length; j++) {
-
-      if (firstTemp.indexOf(firstTemp[i]) === secondTemp.indexOf(secondTemp[j])) {
-        output.push(firstTemp[i]);
-        output.push(secondTemp[j]);
-      }
-    }
+  for (var i = 0; i < first.length; i++) {
+    outPut.push([].concat(first[i], second[i]));
   }
-  for (var k = 0; k < output.length; k += 2) {
-    var newArr = output.slice(k, k + 2);
-
-    finalOutput.push(newArr);
-  }
-  // console.log(finalOutput);
-  return finalOutput;
+  return outPut;
 }
