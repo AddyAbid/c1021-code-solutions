a9b1456a8dbf7c6bd053ef6c0d534b4fa36919e6
Addy Abid <addy.abeed@gmail.com>
2021-12-09 03:11:43 +0000

    Completed React Elements

---
+++ react-elements/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const hiReact = React.createElement('h1', [null], 'Hello, React!');
+ReactDOM.render(hiReact, document.getElementById('root'));
