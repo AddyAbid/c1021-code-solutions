a228dec6cf83cb21479df6b0b28c4263f5e8e657
Addy Abid <addy.abeed@gmail.com>
2021-12-09 22:48:37 +0000

    Completed React Function Components

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CustomButton(props) {
+  return (
+    <button>Click Me!</button>
+  );
+}
+const $root = document.querySelector('#root');
+
+ReactDOM.render(<CustomButton />, $root);
