92d98b9a1237db08269d89cf7619e44ac5ebd822
Addy Abid <addy.abeed@gmail.com>
2021-12-14 21:07:16 +0000

    re wrote logic and updated modal size

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -7,35 +7,28 @@ class AppDrawer extends React.Component {
       drawerOpen: false,
       showMenu: true
     };
-    this.HandleClickDrawer = this.HandleClickDrawer.bind(this);
+    this.handleClickDrawer = this.handleClickDrawer.bind(this);
   }
 
-  HandleClickDrawer() {
-    if (!this.state.drawerOpen) {
-      this.setState({
-        drawerOpen: true,
-        showMenu: false
-      });
-    } else {
-      this.setState({
-        drawerOpen: false,
-        showMenu: true
-      });
-    }
+  handleClickDrawer() {
+    this.setState({
+      drawerOpen: !this.state.drawerOpen,
+      showMenu: !this.state.showMenu
+    });
   }
 
   render() {
     return (
       <div>
-        <i className="fas fa-bars fa-2x" onClick={this.HandleClickDrawer} id={this.state.showMenu ? 'open' : 'hidden'}></i>
+        <i className="fas fa-bars fa-2x" onClick={this.handleClickDrawer} id={this.state.showMenu ? 'open' : 'hidden'}></i>
       <div className="row" id={this.state.drawerOpen ? 'open' : 'hidden'}>
         <div className="column-25">
           <h2>Menu</h2>
-            <a className="block mb" onClick={this.HandleClickDrawer}>About</a>
-            <a className="block mb" onClick={this.HandleClickDrawer}>Get Started</a>
-            <a className="block mb" onClick={this.HandleClickDrawer}>Sign In</a>
+            <a className="block mb" onClick={this.handleClickDrawer}>About</a>
+            <a className="block mb" onClick={this.handleClickDrawer}>Get Started</a>
+            <a className="block mb" onClick={this.handleClickDrawer}>Sign In</a>
         </div>
-          <div className="column-75 overlay" onClick={this.HandleClickDrawer}>
+          <div className="column-75 overlay" onClick={this.handleClickDrawer}>
         </div>
       </div>
       </div>
