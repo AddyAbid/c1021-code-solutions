f5eff9a3d2166601d433867c73e9963f4d28b18f
Addy Abid <addy.abeed@gmail.com>
2021-12-17 22:59:15 +0000

    added dot click function and started timer

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -4,19 +4,26 @@ class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokeIndex: 0
+      pokeIndex: 0,
+      dotIndex: 0,
+      activeInterval: true
     };
     this.clickRight = this.clickRight.bind(this);
     this.clickLeft = this.clickLeft.bind(this);
+    this.dotClick = this.dotClick.bind(this);
+    this.nextImage = this.nextImage.bind(this);
+
   }
 
   clickRight() {
     this.setState({
       pokeIndex: this.state.pokeIndex + 1
+
     });
     if (this.state.pokeIndex > 3) {
       this.setState({
         pokeIndex: 0
+
       });
     }
   }
@@ -24,53 +31,79 @@ class Carousel extends React.Component {
   clickLeft() {
     this.setState({
       pokeIndex: this.state.pokeIndex - 1
+
     });
     if (this.state.pokeIndex < 1) {
       this.setState({
         pokeIndex: 4
+
       });
     }
   }
 
+  dotClick(event) {
+    const idNum = Number(event.target.id);
+    this.setState({
+      pokeIndex: idNum
+    });
+  }
+
+  nextImage() {
+    this.setState({
+      pokeIndex: this.state.pokeIndex + 1
+    });
+    if (this.state.pokeIndex >= 4) {
+      this.setState({
+        pokeIndex: 0
+
+      });
+
+    }
+    // console.log(this.state.pokeIndex);
+  }
+
+  // componentDidMount() {
+  //   if (this.state.activeInterval) {
+  //     setInterval(this.nextImage, 1000);
+  //   }
+  // }
+
   render() {
-    const poke = (
-      <div className="container">
-      {
-        pokemon.map((poke, index) => {
 
-          return (
-            <div key={poke.number}>
-              <div className="row align-center" id={this.state.pokeIndex === index ? 'open' : 'hidden'}>
-                <div className="col-fourth">
-                  <i className="fas fa-chevron-left fa-3x ml" onClick={this.clickLeft}></i>
-                </div>
-                <div className="col-half" >
-                  <img src={poke.img} />
-                  <div className="text-align-center">
-                  </div>
-                </div>
-                <div className="col-fourth">
-                  <i className="fas fa-chevron-right fa-3x right-arrow mr" onClick={this.clickRight}></i>
-                </div>
-              </div>
-            <div className=" poke">
-
-                <i className={this.state.pokeIndex === index ? 'fas fa-circle' : 'far fa-circle'}></i>
-
-              </div>
-            </div>
-
-          );
-        })
-
-      }
-      </div>
-    );
     return (
+      <div className="container">
+      <div>
+        <div className="row align-center" id={this.state.pokeIndex}>
+          <div className="col-fourth">
+            <i className="fas fa-chevron-left fa-3x ml" onClick={this.clickLeft}></i>
+          </div>
+          <div className="col-half" >
+            <img src={pokemon[this.state.pokeIndex].img} />
+            <div className="text-align-center">
+            </div>
+          </div>
+          <div className="col-fourth">
+            <i className="fas fa-chevron-right fa-3x right-arrow mr" onClick={this.clickRight}></i>
+          </div>
+        </div>
 
-      poke
+            <div className='dots'>
+              {
+              pokemon.map((dots, index) => {
+
+                return (
+                  <i onClick={this.dotClick} key={dots.number} id={index} className={this.state.pokeIndex === index
+                    ? 'fas fa-circle pd-5'
+                    : 'far fa-circle pd-5' }></i>
+                );
+              })
+              }
+            </div>
+        </div>
+      </div>
 
     );
+
   }
 }
 
